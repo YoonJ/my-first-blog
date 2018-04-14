7a689e1d916ad9ea263f3e8e8b59179d49e3dd54
yoonJ <oyoons95@gmail.com>
2018-04-15 01:26:03 +0900

    views.py update

--- app/views.py
+++ app/views.py
@@ -15,13 +15,21 @@ def keyboard(request):
 
 @csrf_exempt
 def message(request):
-
-    if(request == None):
-        print("request없어서 문제")
-        
     #request를 한글호환이 되는 'utf-8'로 바뚜어 json_str변수에 저장
-    json_str = ((request.body).decode('uft-8'))
+    json_str = ((request.body).decode('utf-8'))
     #JSON문자열로 되어 있는 것을 Python 타입으로 변경시켜 received_json변수에 저장
+    if(json_str == ''):
+
+        return JsonResponse(
+            {
+                'message': {
+                    'text': 'None'
+                },
+                'keyboard': {
+                    'type': 'text'
+                }
+            })
+
     json_data = json.loads(json_str)
     #사용자가 보낸 명령어에서 필드명이 content인 변수에 저장
     data = json_data['content']
