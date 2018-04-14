9f044decdf0a9b7b75d5f6ac3063f1026e7bb014
yoonJ <oyoons95@gmail.com>
2018-04-15 00:56:30 +0900

    view.py update

--- app/views.py
+++ app/views.py
@@ -13,37 +13,48 @@ def keyboard(request):
         }
     )
 
-@csrf_exempt
+#@csrf_exempt
 def message(request):
 
     #request를 한글호환이 되는 'utf-8'로 바뚜어 json_str변수에 저장
-    json_str = (request.body).decode('uft-8')
+    json_str = ((request.body).decode('uft-8'))
     #JSON문자열로 되어 있는 것을 Python 타입으로 변경시켜 received_json변수에 저장
-    received_json = json.loads(json_str)
+    json_data = json.loads(json_str)
     #사용자가 보낸 명령어에서 필드명이 content인 변수에 저장
-    content_name = received_json['content']
+    data = json_data['content']
 
-    request_mode = content_name.encode('utf-8')
+    #request_mode = data.encode('utf-8')
 
-    if request_mode == '일정':
+    if data == '일정':
         return JsonResponse(
         {
             'message':{
-                'text': "일정을 말했습니다."
+                'text': '일정을 말했습니다.'
             },
             'keyboard':{
                 'type':'text'
             }
         }
     )
-    elif request_mode == '공유':
+    elif data == '공유':
         return JsonResponse(
         {
             'message':{
-                'text': "공유를 말했습니다."
+                'text': '공유를 말했습니다.'
             },
             'keyboard':{
                 'type':'text'
             }
         }
     )
+    else:
+        return JsonResponse(
+            {
+                'message': {
+                    'text': 'Nothing'
+                },
+                'keyboard': {
+                    'type': 'text'
+                }
+            }
+        )
